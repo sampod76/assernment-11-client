f7b931ac0db8878d6c020ce3b1eaa65f457e48c8
sampod76 <108483839+sampod76@users.noreply.github.com>
2022-11-08 00:12:09 +0600

    add react-router-dom and additional equipment

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import { useState, useEffect } from 'react';
+import * as ReactDOM from "react-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+function App() {
+ 
+  return (
+    <div className="">
+    <h1 className='text-5xl'>hello text this file </h1>
+    </div>
+  );
+}
+
+export default App;
