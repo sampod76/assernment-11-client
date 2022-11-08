2701a25326696e60e5d2d52b60f7fc1b6f80882a
sampod76 <108483839+sampod76@users.noreply.github.com>
2022-11-08 16:07:09 +0600

    homepage to add carosal

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -31,7 +31,45 @@ const Home = () => {
                     }
                 </div>
             </div>
-           
+            <div className='flex justify-center'>
+                <Link to='/delivery' className='w-fit buttonb' >See all ..... </Link>
+            </div>
+            <div className='bg-cyan-400 block md:flex  '>
+                <div className='flex items-center' >
+                    <img className='h-24 rounded-lg ' src="https://media.tenor.com/GzOGkwM4EvcAAAAM/popcorn-delivery.gif" alt=""  />
+                </div>
+
+                <div className='h-2/4 w-[40%] mx-auto my-2 '>
+                    <div className="carousel w-full ">
+                        <div id="slide1" className="carousel-item relative w-full">
+                            <img src={`${services[1]?.picture}`} className="w-full rounded-t-lg" />
+                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                <a href="#slide3" className="btn btn-circle">❮</a>
+                                <a href="#slide2" className="btn btn-circle">❯</a>
+                            </div>
+                        </div>
+                        <div id="slide2" className="carousel-item relative w-full">
+                            <img src={`${services[2]?.picture}`} className="w-full" />
+                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                <a href="#slide1" className="btn btn-circle">❮</a>
+                                <a href="#slide3" className="btn btn-circle">❯</a>
+                            </div>
+                        </div>
+                        <div id="slide3" className="carousel-item relative w-full">
+                            <img src={`${services[0]?.picture}`} className="w-full" />
+                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                <a href="#slide2" className="btn btn-circle">❮</a>
+                                <a href="#slide1" className="btn btn-circle">❯</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className='flex items-center ' >
+                    <img className='h-24 rounded-lg' src="https://media.tenor.com/GzOGkwM4EvcAAAAM/popcorn-delivery.gif" alt="" />
+                </div>
+            </div>
 
         </div>
     );
