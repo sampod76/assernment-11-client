f90c033c63e4efc7f30c9dd64767e316a0de66db
sampod76 <108483839+sampod76@users.noreply.github.com>
2022-11-08 16:20:11 +0600

    fix and add route page

---
+++ src/Components/Routes/Routes.js
@@ -0,0 +1,64 @@
+import SingleService from "../PrivitePage/AddService";
+import AddService from "../PrivitePage/AddService";
+import ReviewAll from "../PrivitePage/review/ReviewAll";
+import ReviewCard from "../PrivitePage/review/ReviewCard";
+import EnvelideRout from "../Share/404Rout";
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Home } = require("../Home/Home");
+const { default: Main } = require("../Mian/Main");
+const { default: Delivery } = require("../PrivitePage/Delivery/Delivery");
+const { default: Bloge } = require("../Share/Bloge");
+const { default: Login } = require("../Share/Login");
+const { default: Register } = require("../Share/Register");
+
+export const Routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+
+                element: <Home></Home>
+            },
+            {
+                path: '/home',
+
+                element: <Home></Home>
+            },
+            {
+                path: '/delivery',
+                element: <Delivery></Delivery>
+            },
+            {
+                path: '/delivery/:id',
+                element: <SingleService></SingleService>
+            },
+            {
+                path: '/reviews',
+                element: <ReviewAll></ReviewAll>
+            },
+            {
+                path: '/reviews/:id',
+                element: <ReviewCard></ReviewCard>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/blog',
+                element: <Bloge></Bloge>
+            }
+        ]
+    },
+    {
+        path: '*',
+        element: <EnvelideRout></EnvelideRout>
+    }
+
+])
